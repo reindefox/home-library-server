3ca915434be9375ff25aa8b5e7b64cead39a34f0
reindefox <lev_gera@mail.ru>
2024-05-03 21:57:43 +0300

    Initial commit

---
+++ src/main/java/com/reindefox/homelibraryserver/HomeLibraryServerApplication.java
@@ -0,0 +1,13 @@
+package com.reindefox.homelibraryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLibraryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLibraryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reindefox/homelibraryserver/HomeLibraryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reindefox.homelibraryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLibraryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
